2990f0cb719397c318f241a1b41c181119bbe172
aso1501143 <1501143@st.asojuku.ac.jp>
2017-06-13 10:35:16 +0900

    少し変更

--- src/model/User.java
+++ src/model/User.java
@@ -3,19 +3,27 @@ package model;
 import java.io.Serializable;
 
 public class User implements Serializable{
+	//変数
 	private String id;
 	private String pass;
 
+	//デフォルトコンストラクタ
 	public User(){}
+	
+	//アクセッサ
 	public User(String id,String pass){
 		this.id = id;
 		this.pass = pass;
 	}
+	
 	public String getId(){return id;}
+	
 	public String getPass(){return pass;}
+	
 	public void setUserId(String id) {
 		this.id = id;
 	}
+	
 	public void setPassword(String pass) {
 		this.pass = pass;
 	}
