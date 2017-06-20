4491641cdaaa60045586b34197d269e05357f668
aso1501143 <1501143@st.asojuku.ac.jp>
2017-06-20 10:37:49 +0900

    DB接続設定

--- src/dao/RegisterDao.java
+++ src/dao/RegisterDao.java
@@ -1,5 +1,41 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class RegisterDao {
+	// データベースソース
+	DataSource ds = null; // データベース接続情報
+	Connection con = null;
+	PreparedStatement stmt = null;
+	ResultSet rs = null;
+
+	public Connection connection() throws Exception {
+		// 
+		if (ds == null) {
+			ds = (DataSource) (new InitialContext()).lookup("java:comp/env/jdbc/MySQL");
+		}
+		con = ds.getConnection();
+
+		return con;
+	}
+
+	public void close() throws Exception {
+		// 
+		if (rs != null) {
+			rs.close();
+		}
+		if (stmt != null) {
+			stmt.close();
+		}
+		if (con != null) {
+			con.close();
+		}
+	}
+
 
 }
